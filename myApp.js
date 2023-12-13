caa7784933273451c8224c379d1d9cc08b1195e5
Desmond51 <desmondtekum9@gmail.com>
2023-12-13 14:48:31 +0100

    Creating a REST api

--- myApp.js
+++ myApp.js
@@ -2,11 +2,13 @@ let express = require('express');
 let app = express();
 
 
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname + "/views/index.html");
+app.get('/json', (req, res)=>{
+    res.json({"message": "Hello json"});
 })
 
-app.use("/public", express.static(__dirname + "/public"));
+app.use(
+  express.static("https://boilerplate-express-apvp.onrender.com/json")
+);
 
 
 
