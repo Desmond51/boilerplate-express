5ca55f3e7bc9f797cd840a13374ba87b455605ea
Desmond51 <desmondtekum9@gmail.com>
2023-12-13 14:13:16 +0100

    Made some changes

--- myApp.js
+++ myApp.js
@@ -2,11 +2,11 @@ let express = require('express');
 let app = express();
 
 
-// app.get('/', (req, res)=>{
-//     res.sendFile(__dirname + "/views/index.html");
-// })
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + "/views/index.html");
+})
 
-app.use("/public", express.static("__dirname + /public"));
+app.use("/public", express.static(__dirname + " /public"));
 
 
 
