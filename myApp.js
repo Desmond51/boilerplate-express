d34b6bfa91992f18e990deab12a0e0016a6b7581
Desmond51 <desmondtekum9@gmail.com>
2023-12-13 13:55:41 +0100

    Used the static middleware

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ app.get('/', (req, res)=>{
     res.sendFile(__dirname + "/views/index.html");
 })
 
-app.use("__dirname + /public", express.static());
+app.use("/public", express.static("__dirname + /public"));
 
 
 
